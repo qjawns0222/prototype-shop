9cd251026044e57d48b323060e64627ee1b7bc82
qjawns0222 <33923212+qjawns0222@users.noreply.github.com>
2022-02-07 14:30:12 +0900

    디자인 오류

---
+++ src/components/Orders.jsx
@@ -0,0 +1,10 @@
+export default function Prototypes() {
+  return (
+    <aside>
+      <div className="empty">
+        <div className="title">주문이 없다.</div>{" "}
+        <div className="subtitle">클릭해서 주문추가</div>
+      </div>
+    </aside>
+  );
+}
